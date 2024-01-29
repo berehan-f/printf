2cadc7950d15cbc26dc1a8f222c47f902bf62806
Berehanfh <berehanf.hewot@gmail.com>
2024-01-29 17:10:00 +0300

    Create printf.c

---
+++ printf.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _printf - prints a formatted string to the standard output.
+ * @format: directs how to format the string.
+ * Return: the number of characters printed.
+ */
+
+int _printf(char *format, ...)
+{
+	int counter = 0;
+	int buff_size = 1024;
+	char *buffer = malloc(sizeof(char) * buff_size);
+	va_list list;
+
+	buffer[0] = 0;
+	va_start(list, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '%')
+				non_format(buffer, format, &counter, &buff_size);
+			else if (*format == 'd' || *format == 'i')
+				convert_int(buffer, list,  &counter, &buff_size);
+			else if (*format == 'c')
+				convert_char(buffer, list, &counter, &buff_size);
+			else if (*format == 's')
+				convert_string(buffer, list, &counter, &buff_size);
+		}
+		else
+			non_format(buffer, format, &counter, &buff_size);
+		format++;
+	}
+	print_buffer(buffer, counter);
+	free(buffer);
+	return (counter);
+}
