5a4939a4cb9197ebb4de31feb6181e0176911c35
Berehanfh <berehanf.hewot@gmail.com>
2024-01-29 17:30:51 +0300

    Rename string.c to utilities.c

--- utilities.c
+++ utilities.c
@@ -66,3 +66,43 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+/**
+ * _realloc - reallocates a memory block using malloc and free.
+ * @ptr: a pointer to the memory previously allocated with a call to malloc.
+ * @old_size: the size, in bytes, of the allocated space for ptr.
+ * @new_size: the new size, in bytes of the new memory block.
+ * Return: pointer to the newly allocated memory.
+ *         ptr if new_size == old_size.
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i, min;
+	char *new_ptr;
+
+	if (ptr == NULL)
+		return (malloc(new_size));
+	if (ptr != NULL && new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+		return (ptr);
+
+	if (old_size < new_size)
+		min = old_size;
+	else
+		min = new_size;
+
+	new_ptr = malloc(new_size);
+
+	if (new_ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < min; i++)
+		new_ptr[i] = ((char *) ptr)[i];
+
+	free(ptr);
+	return ((void *) new_ptr);
+}
